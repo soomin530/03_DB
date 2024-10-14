26e3b46e29b2b24970d6e42a1361007a56d8fbb8
soomin530 <rnldudnstnalsdl@gmail.com>
2024-10-14 17:00:56 +0900

    SUBQUERY

--- 사용자계정script.sql
+++ 사용자계정script.sql
@@ -13,13 +13,13 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 -- 11G 이전 문법 사용 허용
 
-CREATE USER kh_wsm IDENTIFIED BY kh1234;
--- 계정 생성 구문 (ID : kh_wsm / PW : kh1234) 
+CREATE USER workbook IDENTIFIED BY workbook;
+-- 계정 생성 구문 (ID : workbook / PW : workbook) 
 
-GRANT RESOURCE, CONNECT TO kh_wsm;
+GRANT RESOURCE, CONNECT TO workbook;
 -- 사용자 계정 권한 부여 설정
 -- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
 -- CONNECT : DB에 연결하고 로그인할 수 있는 권한
 
-ALTER USER kh_wsm DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
 -- 객체가 생성될 수 있는 공간 할당량 무제한 지정
