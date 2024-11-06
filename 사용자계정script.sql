df943920bd46deb3186d4cc5aba48455a32690d6
soomin530 <rnldudnstnalsdl@gmail.com>
2024-11-06 17:25:36 +0900

    유저일

--- 사용자계정script.sql
+++ 사용자계정script.sql
@@ -13,15 +13,15 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 -- 11G 이전 문법 사용 허용
 
-CREATE USER todoList_boot IDENTIFIED BY todoList1234;
+CREATE USER boardProject IDENTIFIED BY board1234;
 -- 계정 생성 구문 (ID : workbook / PW : workbook) 
 
-GRANT RESOURCE, CONNECT TO todoList_boot;
+GRANT RESOURCE, CONNECT TO boardProject;
 -- 사용자 계정 권한 부여 설정
 -- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
 -- CONNECT : DB에 연결하고 로그인할 수 있는 권한
 
-ALTER USER todoList_boot DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+ALTER USER boardProject DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
 -- 객체가 생성될 수 있는 공간 할당량 무제한 지정
 
 
